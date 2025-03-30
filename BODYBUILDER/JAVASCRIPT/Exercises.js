2256284d521355b7d33faf2b56d635ac2e1226e4
Samuel Kimathi Mithika <KimathiWebsites@gmail.com>
2025-03-30 11:02:49 +0300

    Commiting Initial JAVASCRIPT Files

---
+++ BODYBUILDER/JAVASCRIPT/Exercises.js
@@ -0,0 +1,34 @@
+
+let intro = document.getElementById("introduction");
+let adv = document.getElementById("Advantages");
+let dis = document.getElementById("Disadvantages");
+
+intro.style.display = "none";
+adv.style.display = "none";
+dis.style.display = "none";
+
+function introduction(){
+    if(intro.style.display == "none"){
+        intro.style.display = "block";
+    }
+    else{
+        intro.style.display = "none";
+    }
+}
+function advantages(){
+    if(adv.style.display == "none"){
+        adv.style.display = "block";
+    }
+    else{
+        adv.style.display = "none";
+    }
+}
+function disadvantages(){
+    if(dis.style.display == "none"){
+        dis.style.display = "block";
+    }
+    else{
+        dis.style.display = "none";
+    }
+}
+
