04eaacf099bd0d1c332e314a33da69a840c21382
Samuel Kimathi Mithika <KimathiWebsites@gmail.com>
2025-03-30 15:06:25 +0300

    Commiting JAVASCRIPT Version 2 Files

--- BODYBUILDER/JAVASCRIPT/Exercises.js
+++ BODYBUILDER/JAVASCRIPT/Exercises.js
@@ -2,10 +2,12 @@
 let intro = document.getElementById("introduction");
 let adv = document.getElementById("Advantages");
 let dis = document.getElementById("Disadvantages");
+let BarbellExe = document.getElementById("BarbellExe");
 
 intro.style.display = "none";
 adv.style.display = "none";
 dis.style.display = "none";
+BarbellExe.style.display = "none";
 
 function introduction(){
     if(intro.style.display == "none"){
@@ -29,6 +31,15 @@ function disadvantages(){
     }
     else{
         dis.style.display = "none";
+    }
+}
+
+function barbellExe(){
+    if(BarbellExe.style.display == "none"){
+        BarbellExe.style.display = "block";
+    }
+    else{
+        BarbellExe.style.display = "none";
     }
 }
 
